1ad5179f28ba87d127a5f8411fa547ae5f58a848
Lakshanika T G K <lakshanika2006@gmail.com>
2026-07-20 07:05:47 +0000

    Add pattern5.java

---
+++ pattern5.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class pattern5{
+    public static void main(String[] args){
+        int n = 5;
+
+    for(int i=1;i<=n;i++){
+        int p=1;
+    
+        for(int j=i;j<=n;j++){
+        System.out.print(p++ +" ");
+        }
+        System.out.println();
+        }
+    }
+}    
