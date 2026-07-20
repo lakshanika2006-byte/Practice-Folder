13fa71a59e69f3c353c0e2b01da1dad9b1fe2582
Lakshanika T G K <lakshanika2006@gmail.com>
2026-07-20 06:45:56 +0000

    Add pattern3 program

---
+++ pattern3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class pattern3{
+    public static void main(String[] args){
+        int n = 5;
+
+    for(int i=1;i<=n;i++){
+        int p=1;
+        for(int j=1;j<=i;j++){
+        System.out.print(p++ +" ");
+        }
+        System.out.println();
+        }
+    }
+}    
